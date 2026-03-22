656b549c057069be5c7ae019b03189cacaf34462
Joshua Hill <jjhi11240@gmail.com>
2026-03-22 14:53:55 -0700

    Fix 1

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,4 @@
-import "./about.css";
+import "./About.css";
 // import "../index.css";
 import { useState, useEffect } from "react";
 import JoshBodyPhoto from "../assets/images/AboutCascade/Jheadshot1.png"
